c79dc6d85d1b6612759e0b86e7f7d9aed3bc903e
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-06 21:47:44 +0100

    refactored get_crypto_info function

--- load_data.py
+++ load_data.py
@@ -6,7 +6,11 @@ def get_crypto_info(crypto_id: str) -> list[dict]:
     url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin{
         crypto_id}"
 
-    response = requests.get(url)
-    response.raise_for_status()  # Raise an error if the request fails
-    data = response.json()
-    return data
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error if the request fails
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return None
