53f25dc8de32be57d5002a876bb2d47fe96da94c
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-06 21:37:15 +0100

    made test_get_crypto_info_success

--- test_load_data.py
+++ test_load_data.py
@@ -7,3 +7,16 @@ from load_data import get_crypto_info
 def mock_requests_get(mocker):
     """Fixture to mock requests.get."""
     return mocker.patch('requests.get')
+
+
+def test_get_crypto_info_success(mock_requests_get):
+    """Test case for a successful API call."""
+    mock_response = [{"id": "bitcoin", "symbol": "btc", "name": "Bitcoin",
+                      "image": "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"}]
+    mock_requests_get.return_value.status_code = 200
+    mock_requests_get.return_value.json.return_value = mock_response
+
+    result = get_crypto_info("bitcoin")
+
+    assert isinstance(result, list)
+    assert all(isinstance(item, dict) for item in result)
