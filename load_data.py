909f29e29348add2067e37252f79f28918469dd4
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-06 21:39:51 +0100

    made get_crypto_info func

--- load_data.py
+++ load_data.py
@@ -3,4 +3,10 @@ import requests
 
 def get_crypto_info(crypto_id: str) -> list[dict]:
     """Get's information for a crypto currency from the Coingecko API"""
-    ...
+    url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin{
+        crypto_id}"
+
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an error if the request fails
+    data = response.json()
+    return data
